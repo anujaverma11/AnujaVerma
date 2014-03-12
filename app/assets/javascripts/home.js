47d5ba31768e2cea280218e599bf45f0abec3222
Nathan Westgarth <nathan.westgarth@gmail.com>
2014-03-12 23:19:38 +0000

    incuded home.js

---
+++ app/assets/javascripts/home.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+  $.fn.fullpage({
+    menu: '.navbar',
+    verticalCentered: true,
+    resize : false,
+    anchors:['firstPage', 'secondPage', 'thirdPage', 'fourthPage'],
+		navigation: true,
+		navigationPosition: 'right',
+		navigationTooltips:['firstPageTooltip', 'secondPageTooltip', 'thirdPageTooltip', 'fourthPageTooltip'],
+		css3: true
+  });
+});
